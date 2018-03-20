b3b7bc445b92e095fc7ca298e2a8195ae9d67807
zhenyu <zhu.zheny@husky.neu.edu>
2018-03-20 19:52:39 -0400

    sbt configure and show colum

--- CSYE7200_Final_Project/build.sbt
+++ CSYE7200_Final_Project/build.sbt
@@ -4,9 +4,11 @@ version := "1.0"
 
 scalaVersion := "2.11.12"
 
+val sparkVer = "2.1.0"
+
 libraryDependencies ++= {
-  val sparkVer = "2.1.0"
   Seq(
-    "org.apache.spark" %% "spark-core" % sparkVer
+    "org.apache.spark" %% "spark-core" % sparkVer,
+    "org.apache.spark" %% "spark-sql" % sparkVer
   )
 }
--- CSYE7200_Final_Project/src/main/scala/edu/neu/coe/CSYE7200_Team6_2018Spring/ShowColum.scala
+++ CSYE7200_Final_Project/src/main/scala/edu/neu/coe/CSYE7200_Team6_2018Spring/ShowColum.scala
@@ -1,24 +1,31 @@
 package edu.neu.coe.CSYE7200_Team6_2018Spring
 
-import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.SparkSession
 
 
 object ShowColum extends App {
 
-
-
-  def showCol(path: String): Seq[Option[String]] = {
-    val sparkConf = new SparkConf().setAppName("showcol").setMaster("local")
-
-    val sc = new SparkContext(sparkConf)
-    val src = sc.textFile(path)
-    val head = src.take(1)
-    val res = head.toSeq.map(Option[String](_))
-    res.map(println(_))
-    return res
-  }
-
   override def main(args: Array[String]): Unit = {
-    showCol("s3://csye7200.bucket.forfinal/aggregate/agg_match_stats_4.csv")
+    implicit val spark = {
+      println("initial spark")
+      SparkSession
+        .builder()
+        .appName("ShowCol")
+        .master("local[*]")
+        .getOrCreate()}
+
+
+    spark.conf.set("fs.s3n.awsAccessKeyId","AKIAJYRJRH6MYNFWM5CA")
+    spark.conf.set("fs.s3n.awsSecretAccessKey", "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")
+
+    def showCol(path: String): Seq[Option[String]] = {
+      val src = spark.read.textFile(path)
+      val head = src.take(1)
+      val res = head.toSeq.map(Option[String](_))
+      res.map(println(_))
+      return res
+    }
+
+    showCol("s3n://csye7200.bucket.forfinal/aggregate/agg_match_stats_4.csv")
   }
 }
