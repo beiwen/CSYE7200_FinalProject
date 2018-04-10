2601e8bce5ec9b5e4790eff3b3fb898211d60d6c
beiwen <guo.bei@husky.neu.edu>
2018-04-10 00:16:27 -0400

    add test

--- CSYE7200_Final_Project/src/test/scala/edu/neu/coe/CSYE7200_Team6_2018Spring/Ingest/IngestSpec.scala
+++ CSYE7200_Final_Project/src/test/scala/edu/neu/coe/CSYE7200_Team6_2018Spring/Ingest/IngestSpec.scala
@@ -1,24 +1,43 @@
 package edu.neu.coe.CSYE7200_Team6_2018Spring.Ingest
 
+import org.apache.spark.sql.types.{IntegerType, StructField, StructType}
 import org.apache.spark.sql.{Dataset, SparkSession}
 import org.scalatest.{FlatSpec, Matchers}
 
+
 class IngestSpec extends FlatSpec with Matchers {
 
-//  behavior of "ingest"
-//  it should "work for Player" in {
-//    val src = "sample.csv"
-//    implicit val spark = {
-//      println("initial spark")
-//      SparkSession
-//        .builder()
-//        .appName("IngestRawToDataset")
-//        .master("local[*]")
-//        .getOrCreate()
-//    }
-//    val dataset = Ingest.ingest(spark, src, Ingest.schema)
-//    val killsNumList = for (p <- dataset take (20)) yield p.player_kills
-//    val res = for (k <- killsNumList; if k == 1) yield k
-//    res.size shouldBe 5
-//  }
+  behavior of "Ingest"
+  it should "work for Int" in {
+    val schema: StructType = new StructType(Array(StructField("number", IntegerType, true)))
+    val path = "testForInt.csv"
+    implicit val spark = {
+      SparkSession
+        .builder()
+        .appName("IngestRawToDataset")
+        .master("local[*]")
+        .getOrCreate()
+    }
+
+    trait IngestInt extends Ingest {
+      import spark.implicits._
+      def ingest(srcDir: String, schema: StructType)(implicit spark:SparkSession): Dataset[Int] = {
+        spark.read
+          .option("header", "true")
+          .option("inferSchema", "false")
+          .schema(schema)
+          .format("csv")
+          .load(srcDir)
+          .as[Int]
+      }
+    }
+    object IngestInt extends IngestInt
+    val ids = IngestInt.ingest(path,schema)
+    ids.count() shouldBe 14
+    ids.distinct().count() shouldBe 10
+    val total = ids.collect().foldLeft(0)(_ + _)
+    total shouldBe 516
+    spark.stop()
+  }
+
 }
--- CSYE7200_Final_Project/src/test/scala/edu/neu/coe/CSYE7200_Team6_2018Spring/Ingest/PlayerSpec.scala
+++ CSYE7200_Final_Project/src/test/scala/edu/neu/coe/CSYE7200_Team6_2018Spring/Ingest/PlayerSpec.scala
@@ -1,5 +1,5 @@
 package edu.neu.coe.CSYE7200_Team6_2018Spring.Ingest
 
-class PlaySpec {
+class PlayerSpec {
 
 }
