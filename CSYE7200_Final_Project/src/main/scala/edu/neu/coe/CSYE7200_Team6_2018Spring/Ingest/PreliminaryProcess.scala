b40de7493a22f725d53cac5077c0052038327861
beiwen <guo.bei@husky.neu.edu>
2018-04-10 23:03:16 -0400

    add test

--- CSYE7200_Final_Project/src/main/scala/edu/neu/coe/CSYE7200_Team6_2018Spring/Ingest/PreliminaryProcess.scala
+++ CSYE7200_Final_Project/src/main/scala/edu/neu/coe/CSYE7200_Team6_2018Spring/Ingest/PreliminaryProcess.scala
@@ -13,59 +13,61 @@ import org.apache.spark.sql.types.StructType
   *  sampling 10000 rows from one of pubg datasource for unit test.
   */
 
-
-object PreliminaryProcess extends App {
-
-  override def main(args: Array[String]): Unit = {
-
+class PreliminaryProcess {
+  //used to read data from a specific path(s3 bucket) into DataFrame,
+  //because need to infer schema, so it is relatively slow
+  def readSrc(format : String, path : String) = {
     implicit val spark = {
       println("initial spark")
       SparkSession
         .builder()
         .appName("PreliminaryProcess")
         .master("local[*]")
-        .getOrCreate()}
-
+        .getOrCreate()
+    }
     //Setting up required configuration for spark to talk with AWS S3 bucket. The following way to provided AWS credential is not recommended.
     //Following code are provided just for users who have no experience with AWS S3.
-
     spark.conf.set("fs.s3n.awsAccessKeyId","AKIAJYRJRH6MYNFWM5CA")
     spark.conf.set("fs.s3n.awsSecretAccessKey", "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")
-
-    val path = "s3n://csye7200.bucket.forfinal/aggregate/agg_match_stats_4.csv"
-
-    lazy val src = spark.read.format("csv")
+    spark.read.format(format)
       .option("header", "true")
       .option("inferSchema", "true")
       .load(path)
 
-    def showCol(): StructType = {
-      val schema = src.schema
-      println(schema)
-      return schema
-    }
-
-    def sampling(rows: Int):Unit = {
-
-      val file = "temp"
-      val desFile = "sample.csv"
-      FileUtil.fullyDelete(new File(file))
-      FileUtil.fullyDelete(new File(desFile))
-
-      val sampleRows = src.limit(rows)
-      sampleRows.coalesce(1).write.option("header", "true").format("com.databricks.spark.csv").save("temp")
-
-      def merge(srcPath: String, dstPath: String): Unit =  {
-        val hadoopConfig = new Configuration()
-        val hdfs = FileSystem.get(hadoopConfig)
-        FileUtil.copyMerge(hdfs, new Path(srcPath), hdfs, new Path(dstPath), false, hadoopConfig, null)
-      }
-
-      merge(file, desFile)
-      FileUtil.fullyDelete(new File(file))
-    }
-
-    val s = showCol()
-    sampling(10000)
   }
+  //used to show the dataFrame' s schema
+  def showCol(src : DataFrame): StructType = {
+    val schema = src.schema
+    println(schema)
+    return schema
+  }
+  //used to merge files from an HDFS directory over to an HDFS file
+  def merge(srcPath: String, dstPath: String): Unit =  {
+    val hadoopConfig = new Configuration()
+    val hdfs = FileSystem.get(hadoopConfig)
+    FileUtil.copyMerge(hdfs, new Path(srcPath), hdfs, new Path(dstPath), false, hadoopConfig, null)
+  }
+
+  //used to take a sample with the specific size from source DataFrame and merge into destination file through merge function
+  def sampling(rows: Int, src: DataFrame, desfile :String):Unit = {
+    val file = "temp"
+    val desFile = desfile
+    FileUtil.fullyDelete(new File(file))
+    FileUtil.fullyDelete(new File(desFile))
+
+    val sampleRows = src.limit(rows)
+    sampleRows.coalesce(1).write.option("header", "true").format("com.databricks.spark.csv").save("temp")
+
+    merge(file, desFile)
+    FileUtil.fullyDelete(new File(file))
+  }
+}
+
+
+object PreliminaryProcess extends App {
+  val path = "s3n://csye7200.bucket.forfinal/aggregate/agg_match_stats_4.csv"
+  val preliminaryProcess = new PreliminaryProcess
+  val src = preliminaryProcess.readSrc("csv", path)
+  preliminaryProcess.showCol(src)
+  preliminaryProcess.sampling(10000, src, "sample.csv")
 }
--- CSYE7200_Final_Project/src/test/scala/edu/neu/coe/CSYE7200_Team6_2018Spring/Ingest/PreliminaryProcessSpec.scala
+++ CSYE7200_Final_Project/src/test/scala/edu/neu/coe/CSYE7200_Team6_2018Spring/Ingest/PreliminaryProcessSpec.scala
@@ -1,7 +1,35 @@
 package edu.neu.coe.CSYE7200_Team6_2018Spring.Ingest
 
+import java.nio.file.{Files, Paths}
+
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.types.{DoubleType, IntegerType, StringType}
 import org.scalatest.{FlatSpec, Matchers}
 
-class PreliminaryProcessSpec extends FlatSpec with Matchers{
+class PreliminaryProcessSpec extends FlatSpec with Matchers {
 
+  val preliminaryProcess = new PreliminaryProcess
+  val df = preliminaryProcess.readSrc("csv","sample.csv")
+
+  behavior of "Priliminary Process"
+
+  it should "work for reading source" in {
+    df shouldBe a [DataFrame]
+    df.count() shouldBe 10000
+  }
+
+  it should "work for Schema(Column)" in {
+    val schema = df.schema
+    val names = for(schemafield <- schema) yield schemafield.name
+    val types = for(schemafield <- schema) yield schemafield.dataType
+    names === List("date", "game_size", "match_id", "match_mode", "party_size", "player_assists", "player_dbno", "player_dist_ride", "player_dist_walk", "player_dmg", "player_kills", "player_name", "player_survive_time", "team_id", "team_placement")
+    types === List(StringType, IntegerType, StringType, StringType, IntegerType, IntegerType, IntegerType, DoubleType, DoubleType, IntegerType, IntegerType, StringType, DoubleType, IntegerType, IntegerType)
+  }
+
+  it should "work for Sampling" in {
+    preliminaryProcess.sampling(50, df, "testOfSampling.csv")
+    assert(Files.exists(Paths.get("testOfSampling.csv")))
+    val src = preliminaryProcess.readSrc("csv", "testOfSampling.csv")
+    src.count() shouldBe 50
+  }
 }
