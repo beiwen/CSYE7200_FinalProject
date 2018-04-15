acac175f8560443de5e099420d52642d2c8210ea
beiwen <guo.bei@husky.neu.edu>
2018-04-15 15:24:11 -0400

    implement classification

--- CSYE7200_Final_Project/src/main/scala/edu/neu/coe/CSYE7200_Team6_2018Spring/Ingest/Classification.scala
+++ CSYE7200_Final_Project/src/main/scala/edu/neu/coe/CSYE7200_Team6_2018Spring/Ingest/Classification.scala
@@ -1,5 +1,7 @@
 package edu.neu.coe.CSYE7200_Team6_2018Spring.Ingest
 
+import org.apache.spark.ml.classification.MultilayerPerceptronClassifier
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
 import org.apache.spark.ml.feature.{Normalizer, VectorAssembler}
 import org.apache.spark.sql.{DataFrame, Dataset}
 import org.apache.spark.sql.functions.udf
@@ -11,7 +13,24 @@ object Classification {
       val splits = dataFrame.randomSplit(Array(0.7, 0.3), seed = 1234L)
       val trian =splits(0)
       val test = splits(1)
+      val layers = Array(6, 200, 200, 2)
+      val trainer = new MultilayerPerceptronClassifier()
+        .setLayers(layers)
+        .setLabelCol("survived")
+        .setMaxIter(100)
+        .setSeed(1234L)
+      val model = trainer.fit(trian)
+      val result = model.transform(test)
+      val predictionLabels = result.select("prediction", "survived")
+      val evaluator = new MulticlassClassificationEvaluator().setMetricName("accuracy").setLabelCol("survived")
+      println("Test set accuracy = " + evaluator.evaluate(predictionLabels))
     }
+
+    val soloPlayers = ds.filter(d => d.party_size == 1).cache()
+    val duoPlayers = ds.filter(d => d.party_size == 2).cache()
+    val squadPlayers = ds.filter(d => d.party_size == 4).cache()
+
+    buildModelHelper(soloPlayers)
   }
 
   def createDfWithFeature(ds: Dataset[Player]): DataFrame ={
@@ -22,8 +41,9 @@ object Classification {
     val colArray = Array("player_assists", "player_dbno", "player_dist_ride", "player_dist_walk", "player_dmg", "player_kills")
     val vecAss = new VectorAssembler().setInputCols(colArray).setOutputCol("feature_unnormalized")
     val normalizer = new Normalizer().setInputCol("feature_unnormalized").setOutputCol("features")
-    val assembledDf = vecAss.transform(normalizer.transform(ds))
-    val resultDf = assembledDf.withColumn("survived", isWinnerUdf(assembledDf("team_placement")))
+    val df_temp_1 = vecAss.transform(ds)
+    val df_temp_2 = normalizer.transform(df_temp_1)
+    val resultDf = df_temp_2.withColumn("survived", isWinnerUdf(df_temp_2("team_placement")))
     resultDf
   }
 
